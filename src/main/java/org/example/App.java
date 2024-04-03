ae7cd8fcd2b5ed7ec947e466b41cb58dbf0b1c8c
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2024-04-03 21:41:04 +0500

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * class CountDocsApplication for run application
+ * */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
